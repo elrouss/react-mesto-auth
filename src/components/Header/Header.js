0a2da62fde5da27dd223c7a99baf300ade09764a
Boris Zashliapin <test-zashliapin-b@yandex.ru>
2023-01-24 19:21:58 +0300

    feat: сверстать на десктоп компоненты Login & Register, вынести общий для них компонент WelcomeWindowWithForm, модифицировать Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,40 @@
+import { Outlet } from 'react-router-dom';
 import logo from '../../images/header__logo_theme_light.svg';
 
 export default function Header() {
+  // TODO: Скорее всего, потребуется разделить действия у трех кнопок в шапке
+  const href = window.location.href;
+
+  function renderBtnText() {
+    if (href.endsWith('/sign-up')) {
+      return (
+        'Войти'
+      );
+    };
+
+    if (href.endsWith('/sign-in')) {
+      return (
+        'Регистрация'
+      );
+    };
+
+    if (href.endsWith('http://localhost:3000/')) {
+      return (
+        'Выйти'
+      );
+    }
+  };
+
   return (
-    <header className="header">
-      <img src={logo} alt="Логотип сайта &laquo;Mesto&raquo;" className="header__logo" />
-    </header>
+    <>
+      <header className="header">
+        <img src={logo} alt="Логотип сайта &laquo;Mesto&raquo;" className="header__logo" />
+        <div className='header__data'>
+          {href.endsWith('http://localhost:3000/') && <p className='header__email'>email@mail.com</p>}
+          <button className='header__btn'>{renderBtnText()}</button>
+        </div>
+      </header>
+      <Outlet />
+    </>
   );
-}
+};
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,5 +1,14 @@
+import WelcomeWindowWithForm from "../WelcomeWindowWithForm/WelcomeWindowWithForm";
+
 export default function Register() {
   return (
-    <p>test register</p>
+    <WelcomeWindowWithForm
+      heading={'Регистрация'}
+      btnSubmit={'Зарегистрироваться'}
+    >
+      <p>Уже зарегистрированы?&nbsp;
+        <a className="welcome-window__link" href="http://localhost:3000/sign-up">Войти</a>
+      </p>
+    </WelcomeWindowWithForm>
   );
 };
